6e080605be440e34182bb61b7783f7b18732b28f
Kamel Saadi <saadi.kamel@gmail.com>
2018-08-16 19:47:42 +0000

    Commit 10

---
+++ prob_scenario/solution4_app.py
@@ -0,0 +1,19 @@
+#
+# ---- pyspark app
+#
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setMaster("local").setAppName("myApp")
+sc   = SparkContext(conf = conf)
+
+# loading from local dirctory, we could upload to hdfs. file:/ also works
+# 1 -
+rdd1 = sc.textFile("file:///home/maria_dev/kamel/Spark_Training/prob_scenario/hadoopexama4A.txt")
+rdd2 = sc.textFile("file:///home/maria_dev/kamel/Spark_Training/prob_scenario/hadoopexama4B.txt")
+rdd3 = sc.textFile("file:///home/maria_dev/kamel/Spark_Training/prob_scenario/hadoopexama4C.txt")
+
+# 2 -
+rddAll = rdd1.union(rdd2).union(rdd3)
+
+# 3 - 
+print "There number of words is %i " % rddAll.count()
+
