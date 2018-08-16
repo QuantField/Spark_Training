5cf674fe379ec442d4ca4cf1438fbe83add5e259
Kamel Saadi <saadi.kamel@gmail.com>
2018-08-16 22:46:25 +0000

    Commit 12

---
+++ chap9/sql_spark_tut_1.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.json("file:/home/maria_dev/kamel/Spark_Training/chap9/people.json")
+df.show()
++----+-------+
+| age|   name|
++----+-------+
+|null|Michael|
+|  30|   Andy|
+|  19| Justin|
++----+-------+
